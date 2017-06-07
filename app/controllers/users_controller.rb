a480954a70125dec1e91ced14ad0404621ca30fb
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-07 17:54:10 +0900

    added edit, update, user_params, and redirect methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+before_action :move_to_index, :except => [:index]
 
-def edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+      user = User.find(params [:id])
+      if user.user_id == current_user.id
+        user.update(user_params)
+      end
+  end
+
+  private
+  def user_params
+      params.permit(:name, :email)
+  end
+
+  def move_to_index
+      redirect_to action: :index unless user_signed_in?
+  end
 end
 
-end
