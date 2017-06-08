908ada36d8b9e44315c0e0513ff137e6067dc5d9
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-08 13:07:07 +0900

    added gem "pry-rails"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'messages#index'
-
    # User情報編集機能 no.6 ルートの設定？
     resources :users, only: [:edit, :update]
    # get '' => '#edit'
