22c7a39c5c38d3004c46fa97d1d106f360accc82
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-08 12:18:14 +0900

    added action after editing the user info
    and when user could not change its their info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,16 @@ before_action :move_to_index, :except => [:index]
   def update
     user = User.find(params [:id])
     if user.id == current_user.id
-      user.update(user_params)
+       user.update(name:user_params[:name], email:user_params[:email])
+       move_to_index
     else
-      move_to_index
+       redirect_to actin: :edit
     end
   end
 
   private
   def user_params
-      params.permit(:name, :email)
+      params.require(:user).permit(:name, :email)
   end
 
   def move_to_index
