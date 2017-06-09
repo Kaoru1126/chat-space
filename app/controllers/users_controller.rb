882c14aef7f50d3e03c02ea862c51a99c53238dd
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-09 17:40:30 +0900

    set strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,29 @@ class UsersController < ApplicationController
  before_action :move_to_sign_in
  before_action :making_user
 
-   def edit
-   end
-
+  def edit
+  end
 
   def update
     if @user == current_user.id
        user.update(name:user_params[:name], email:user_params[:email])
-    end
+      end
   end
 
   private
+  def making_user
+     @user = User.find(params[:id])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+  end
+
   def user_params
-      params.require(:user).permit(:name, :email, :id)
+      params.require(:user).permit(:name, :email)
   end
 
   def move_to_sign_in
     redirect_to new_user_session unless user_signed_in?
   end
-
-  def making_user
-    @user = User.find(params[:id])
-  end
-
-
 end
