b8c1ae9bb880ea7639eddde19ba29fe9e60d0f85
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-09 16:13:46 +0900

    fix certain prob
    could not go to sign-in page and authenticate_user did not work well
    => deleted “except [index] or something”
    
    this controller affects on every controller, so be careful about
    writing something on this.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
     def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
