e593914ebb9ac704143a5e0dba570f0da1adb403
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-09 20:47:26 +0900

    omitted some texts to replace them to application_contro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :move_to_sign_in unless user_signed_in?
 
     def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
     end
+
+    def move_to_sign_in
+      user_session
+    end
+
 end
-
-
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,4 @@
 class MessagesController < ApplicationController
-  # before_action :move_to_sign_in unless user_signed_in?
-
-  def move_to_sign_in
-      user_session
-  end
 
 end
 
