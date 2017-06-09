c36cef1253a5775cb0e61b10a4350d739c1003b3
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-09 20:09:34 +0900

    few changed by the comment i got
    1) def name for “making_user”
    2)paramater’s name
    3) redirect to render
    4) in the def move to sign in’s way to call the method/
    5)omitted the def “configure…” to the application contr…

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   # before_action :move_to_sign_in unless user_signed_in?
 
   def move_to_sign_in
-    redirect_to new_user_session
+      user_session
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,22 @@
 class UsersController < ApplicationController
  before_action :move_to_sign_in
- before_action :making_user
+ before_action :set_user
 
   def edit
   end
 
   def update
     if @user == current_user
-       @user.update(name:user_params[:name], email:user_params[:email])
+       @user.update(user_params)
        redirect_to controller: :messages, action: :index
       else
-      redirect_to edit_user_path(current_user)
+       render action: :edit
       end
   end
 
   private
-  def making_user
+  def set_user
      @user = User.find(params[:id])
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
   end
 
   def user_params
@@ -28,6 +24,6 @@ class UsersController < ApplicationController
   end
 
   def move_to_sign_in
-    redirect_to new_user_session unless user_signed_in?
+      user_session
   end
 end
