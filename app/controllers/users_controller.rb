614d972939724f2d031c08c1823446e79e2f93f2
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-09 16:09:36 +0900

    added before action and method for the action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,29 @@
 class UsersController < ApplicationController
-before_action :move_to_index, :except => [:index]
+ before_action :move_to_sign_in
+ before_action :making_user
 
-  def edit
-    @user = User.find(params[:id])
-  end
+   def edit
+   end
+
 
   def update
-    user = User.find(params[:id])
-    if user == current_user.id
+    if @user == current_user.id
        user.update(name:user_params[:name], email:user_params[:email])
     end
   end
 
   private
   def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :id)
   end
 
-  def move_to_index
-      redirect_to action: :index unless user_signed_in?
+  def move_to_sign_in
+    redirect_to new_user_session unless user_signed_in?
   end
+
+  def making_user
+    @user = User.find(params[:id])
+  end
+
+
 end
-
