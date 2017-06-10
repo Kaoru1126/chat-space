d4e3a0018a85badb0d3c72baa0685a268122de6c
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-10 19:04:27 +0900

    set "null false"

--- db/migrate/20170610065959_create_groups.rb
+++ db/migrate/20170610065959_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
 
-      t.string   "name"
+      t.string   :name, null: false
       t.timestamps
     end
   end
