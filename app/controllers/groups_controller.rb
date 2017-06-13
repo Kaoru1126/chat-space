3687fff7f8709e23d61b43d24c4e3e66986c4c87
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-13 20:56:57 +0900

    making actions in group_contro

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,30 @@
 class GroupsController < ApplicationController
-
   def new
+    @group = Group.new
+  end
+
+  def create
+    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = "グループを作成しました"
+      redirect_to root_path
+    else
+     flash[:alert] = "グループの作成に失敗しました"
+     render :new
+   end
   end
 
   def edit
   end
 
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def move_to_sign_in
+    user_session
+  end
 end
