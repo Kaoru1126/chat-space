058915abde1d8601eed9fc954df3cd8b5ef5f873
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-15 12:01:47 +0900

    added for some method
    update
    edit
    private
    index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,17 @@
 class GroupsController < ApplicationController
+  before_action :set_group
+
+  def index
+
+  end
 
   def new
     @group = Group.new
     @group.users << current_user
+  end
+
+  def edit
+    @group = Group.find(params[:id])
   end
 
   def create
@@ -15,13 +24,27 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    binding.pry
+    if redirect_to group_messages_path(@group.id), notice: "グループを編集しました。"
+      return
+    else
+      render :index
+    end
   end
 
   private
 
+  def set_group
+    if params[:group_id].present?
+      @group = Group.find(params[:group_id])
+    end
+  end
+
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, {user_ids: []})
   end
 
 end
