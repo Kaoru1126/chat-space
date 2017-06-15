e27a860f40347a6a85ca90056eb43552bbd9c0c2
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-15 11:59:18 +0900

    add actions to messages contr

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,15 @@
 class MessagesController < ApplicationController
+before_action :set_group
 
+  def index
+    @groups = current_user.groups
+  end
+
+
+ private
+  def set_group
+    if params[:group_id].present?
+     @group = Group.find(params[:group_id])
+    end
+  end
 end
