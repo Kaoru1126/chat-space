0faa1ddf75e3a286e5fa87f411974374ecdbcbf1
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-17 17:32:21 +0900

    to validate messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
 
-  validates :body, presence: true || :image, presence: true
+  validates :body, length: { minimum: 1 }, if: :image_nil
+
+  def image_nil
+    image.file.nil?
+  end
 
   mount_uploader :image, ImageUploader
 end
