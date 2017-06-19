29fefadaf538d4f227fa2014e776570109b19c58
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-19 20:31:09 +0900

    to use image file for testing

---
+++ spec/factories/message.rb
@@ -0,0 +1,12 @@
+include ActionDispatch::TestProcess
+
+FactoryGirl.define do
+
+  factory :message do
+    body        { Faker::Lorem.sentence }
+    image       { fixture_file_upload("#{::Rails.root}/spec/fixtures/000panda.jpg", 'image/jpg') }
+    group_id    "1"
+    user_id     "1"
+   end
+ end
+
