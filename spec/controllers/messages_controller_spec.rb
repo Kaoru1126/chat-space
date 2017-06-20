bdd58459e67627b24828e8c226305108a7aee8fe
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-20 20:25:13 +0900

    changed how to write what they test about.
    do not use question style, it’s better to be command style

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -17,12 +17,12 @@ describe MessagesController, type: :controller do
           get :index, params: { group_id: group.id }
         end
 
-        it "アクション内で定義済みの@messageがある？" do
+        it "アクション内で定義済みの@messageがある" do
           message = Message.new
           expect(assigns(:message)).to be_a_new Message
         end
 
-        it "アクション内で定義済み@groupがある？" do
+        it "アクション内で定義済み@groupがある" do
           expect(assigns(:group)).to eq group
         end
 
@@ -31,23 +31,23 @@ describe MessagesController, type: :controller do
           expect(assigns(:messages)).to eq messages
         end
 
-        it "アクション内で定義済み@groupsがある？" do
+        it "アクション内で定義済み@groupsがある" do
           groups = user.groups
           expect(assigns(:groups)).to eq groups
         end
 
-        it "アクション内で定義済みの@usersがある？" do
+        it "アクション内で定義済みの@usersがある" do
           users = group.users
           expect(assigns(:users)).to eq users
         end
 
-        it "インデックス画面に遷移するか？" do
+        it "インデックス画面に遷移する" do
           expect(response).to render_template :index
         end
       end
 
       context 'ログインしてない場合' do
-        it 'ログイン画面にリダイレクトできているか？' do
+        it 'ログイン画面にリダイレクトできた' do
           get :index, params: { group_id: group.id }
           expect(response).to redirect_to new_user_session_path
         end
@@ -61,43 +61,43 @@ describe MessagesController, type: :controller do
       end
 
       context 'ログインしていて、保存できた場合' do
-        it "メッセージの保存はできたのか？" do
+        it "メッセージの保存ができた" do
           expect {
             post :create, params: { message: attributes_for(:message, {body: message.body }),group_id: group.id, user_id: user.id }}.to change(Message, :count).by(1)
         end
 
-        it "投稿成功時にフラッシュメッセージの表示はあるか？" do
+        it "投稿成功時にフラッシュメッセージを表示した" do
           post :create, params: { group_id: group.id, user_id: user.id, message: attributes_for(:message, { body: 'testtest' })}
           expect(flash[:notice]).to include("送信されました")
         end
 
-        it "チャット画面に遷移しているか？" do
+        it "チャット画面に遷移した" do
           post :create, params: { message: attributes_for(:message, { body: message.body }), group_id: group.id, user_id: user.id }
           expect(response).to redirect_to group_messages_path(group.id)
         end
       end
 
       context 'ログインはしているが、保存できなかった場合' do
-        it 'メッセージの保存は行われなかったか？' do
+        it 'メッセージの保存が行われなかった' do
           expect {
             post :create, params: {
                message: attributes_for(:message, { body: nil, image: nil }), group_id: group.id, user_id: user.id } }
                .not_to change(Message, :count)
         end
 
-        it '投稿失敗時にフラッシュメッセージの表示はあるか？' do
+        it '投稿失敗時にフラッシュメッセージの表示があった' do
           post :create, params: { group_id: group.id, user_id: user.id, message: attributes_for(:message, { body: nil, image: nil}) }
           expect(flash[:alert]).to include("メッセージかイメージを入力して下さい")
         end
 
-        it '意図したビューへ遷移するか？' do
+        it '意図したビューへ遷移した' do
           post :create, params: { message: attributes_for(:message, { body: nil, image: nil }), group_id: group.id, user_id: user.id }
           expect(response).to render_template :index
         end
       end
     end
     context 'ログインしていない場合' do
-      it "意図した画面にリダイレクトできているか？" do
+      it "意図した画面にリダイレクトできた" do
         post :create, params: { group_id: group.id, user_id: user.id }
         expect(response).to redirect_to new_user_session_path
       end
