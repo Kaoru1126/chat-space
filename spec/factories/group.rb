17c8623a365f8652d812d3031ffcc94b6326a615
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-20 19:18:36 +0900

    setting for factory girls

---
+++ spec/factories/group.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :group do
+    name { Faker::Name.name }
+  end
+factory :message do
+    name
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+pass = Faker::Internet.password
+  factory :user do
+    name                    Faker::Name.name
+    email                   Faker::Internet.email
+    password                pass
+    password_confirmation   pass
+   end
+end
