a8743272bea1686aab274420161a4132b071b267
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-22 16:36:15 +0900

    minor fix from last change

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
-pass = Faker::Internet.password
+
   factory :user do
-    name                    Faker::Name.name
-    email                   Faker::Internet.email
-    password                pass
-    password_confirmation   pass
+    name                    { Faker::Name.name }
+    email                   { Faker::Internet.email }
+    password                { Faker::Internet.password }
+    password_confirmation   { Faker::Internet.password }
    end
 end
