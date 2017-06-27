3064198f3eba623d137cbc44d00de6fa13ad3a60
Kaoru1126 <chocolate.k1126@gmail.com>
2017-06-27 09:54:06 +0900

    自動更新機能追加
    scrollの微調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,14 @@
 $(function() {
+
   function buildHTML(message) {
-    var messageBody = '<ui><li class="user">' + message.userName + '</li>'
+    var messageBody = '<div class="js-messages" message-id="' + message.id + '">'
+                    + '<ui><li class="user">' + message.userName + '</li>'
                     + '<li class="timestamp">' + message.createdAt + '</li>'
-                    + '<li class="textinchat">' + message.body + '</li>';
+                    + '<li class="textinchat">' + message.body + '</li></div>';
                     if (message.image.url == null) {
                       return messageBody;
                     } else {
-                      return messageBody + "<li><img src=" + message.image.url + "></li>";
+                    return messageBody + "<li><img src=" + message.image.url + "></li>";
     }
   }
 
@@ -19,11 +21,11 @@ $(function() {
 // スクローリングanimateの記述
   function animateScroll() {
     $(".area").animate({
-      scrollTop: $(".js-messages").height()
-    }, 1000, "swing");
+      scrollTop: $(".area")[0].scrollHeight
+    }, "fast");
    }
 
-// フォーム送信で発火する記述
+// 非同期通信の記述
   $('#btninindex').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData($('form').get(0));
@@ -39,15 +41,40 @@ $(function() {
 // done、テキストとファイルを投稿&テキスト入力欄クリア、スクロール呼び出し
     .done(function(message){
       var html = buildHTML(message);
-      $('.start').append(html);
+      $(".area").append(html);
       clearingForm();
       animateScroll();
       })
     .fail(function(){
        alert('error');
       });
-    });
+  });
 
+// 自動更新機能の記述
+  var interval = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    $.ajax({
+      type: 'GET',
+      url: location.href,
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var id = $(".js-messages").last().data("messageid");
+      var addingHTML = '';
+      console.log("更新中");
+      $(".js-messages").remove();
+      data.messages.forEach(function(message) {
+        addingHTML = buildHTML(message);
+        $(".area").append(addingHTML);
+      });
+      var addingHTML = '';
+      animateScroll();
+    })
+      .fail(function(data) {
+        alert('自動更新は失敗しています');
+      });
+    } else {
+    clearInterval(interval);
+    }} , 10 * 1000 );
 });
-
 
